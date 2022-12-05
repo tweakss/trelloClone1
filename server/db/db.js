0eb29db8279b3539bef25fe8b62d515ec5814cfa
tweakss <tweakss@gmail.com>
2022-12-05 02:56:17 -0500

    back to no docker

--- server/db/db.js
+++ server/db/db.js
@@ -21,6 +21,6 @@ if(process.env.DATABASE_URL){
 }
 
 const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://postgres:password@172.19.0.2:5432/${databaseName}`, config)
+  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`, config)
   // process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`, config)
 module.exports = db
