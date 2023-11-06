f95e9a57fc1b088381d4c973fae69ce4154fb467
tweakss <46912181+tweakss@users.noreply.github.com>
2023-11-05 20:51:38 -0500

    Update from recreating DB in Render
    Made new PostgreSQL db in Render then updating:
    const databaseName
    
    process.env.DATABASE_URL || `postgres:// ...`, config)

--- server/db/db.js
+++ server/db/db.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize')
 const pkg = require('../../package.json')
 
 // const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
-const databaseName = "trello_clone_9yoy";
+const databaseName = "trelloclone1_v1_db";
 
 const config = {
   logging: false
@@ -22,7 +22,8 @@ if(process.env.LOGGING === 'true'){
 // }
 
 const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://trello_clone_9yoy_user:wMORfB8kzoXQGiyysFo4JplSh5l7888a@dpg-ce7oj85a4990lb62a2sg-a/trello_clone_9yoy`, config)
+  process.env.DATABASE_URL || `postgres://trelloclone1_v1_db_user:ENWYr9dITMrq7Cr7FzRPhrkrkKeS91uk@dpg-cl445oauuipc738q400g-a/trelloclone1_v1_db`, config)
+  // process.env.DATABASE_URL || `postgres://trello_clone_9yoy_user:wMORfB8kzoXQGiyysFo4JplSh5l7888a@dpg-ce7oj85a4990lb62a2sg-a/trello_clone_9yoy`, config)
   // process.env.DATABASE_URL || `postgres://trello_clone_9yoy_user:wMORfB8kzoXQGiyysFo4JplSh5l7888a@dpg-ce7oj85a4990lb62a2sg-a:5432/${databaseName}`, config)
   // process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`, config)
 module.exports = db
